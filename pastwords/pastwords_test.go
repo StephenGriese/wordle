6bdcf110c6c7b180f63edf8cf064b15ecbd25958
Stephen Griese <stephen_griese@comcast.com>
2026-02-06 13:14:37 -0500

    Remove pastwords package (deleted files)

--- pastwords/pastwords_test.go
+++
@@ -1,8 +0,0 @@
-//go:build ignore
-// +build ignore
-
-package pastwords_test
-
-// Historical tests for the removed pastwords package have been archived and are
-// intentionally excluded from the test run using the 'ignore' build tag.
-// See git history for the original test code if needed.
